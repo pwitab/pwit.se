4d03253ddc25ce0b0a820ebfc857c63a60e42ddb
Krolken <Krolken@users.noreply.github.com>
2021-07-15 10:13:04 +0000

    deploy: b7d9af57a43f5e4a74445a6633d17d0299eda1a2

---
+++ _nuxt/static/1626343982/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpwit.se\u002F_nuxt\u002Fstatic\u002F1626343982",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpwit.se\u002F",assetsPath:"\u002Fpwit.se\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ab0ef3af"}}}}(null));
