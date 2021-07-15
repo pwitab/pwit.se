87b7e7e6e6af905c94d02a74e1296d23205a5623
Henrik Palmlund Wahlgren <henrik@pwit.se>
2021-07-15 12:31:39 +0200

    removed router part

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/pwit.se/'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'pwit_se',
