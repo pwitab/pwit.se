12f074ea1ee250b4f083da85ce40876bedd1b0ec
Krolken <Krolken@users.noreply.github.com>
2021-07-15 14:54:35 +0000

    deploy: f093ae0ab674b03909323709485a0ae9d381a807

--- _nuxt/static/1626345163/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626345163",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"95a906ec"}}}}(null,"\u002F"));
